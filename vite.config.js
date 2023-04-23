55aa74318c9f83cd411f638f88201624a8413718
Franck Garcon <franck.garcon@gmail.com>
2023-04-23 09:55:09 +0200

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react({
+    // eslint-disable-next-line no-undef
+    fastRefresh: process.env.NODE_ENV !== 'test'
+  })]
+});
