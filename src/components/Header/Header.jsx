8268995929e2b2d175f928262c25085596373104
Franck Garcon <franck.garcon@gmail.com>
2023-04-26 22:10:18 +0200

    Add Header / Footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,17 @@
 const Header = () => {
-    return (<div className="Header"/>);
+    return (
+        <header>
+            <h1 id="logo">My Logo</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Portfolio</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div className="app">
-            Hello
+
         </div>);
 }
 
